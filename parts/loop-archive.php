9eab184ae9c8a413492767317b01323cf433b6e5
dpeters911 <dpeters911@hotmail.com>
2017-06-09 22:52:36 -0700

    Condensed Archive/Blog Category View
    Utilized the_excerpt to remvoe html/formatting and condense excerpt from
    articles.

--- parts/loop-archive.php
+++ parts/loop-archive.php
@@ -1,21 +1,25 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class('card-gray bdr-stripe-red'); ?> role="article">					
+<article id="post-<?php the_ID(); ?>" <?php post_class('archive-card bdr-stripe-red'); ?> role="article">					
 	<div class="row">
-		<div class="archive-image large-4 medium-3 small-12 columns">
-			<a href="<?php the_permalink() ?>"><?php the_post_thumbnail('full'); ?></a>
+		<div class="archive-item-background large-3 medium-4 small-12 columns">
+			<a href="<?php the_permalink() ?>" class="archive-image-link">
+				<?php 
+					if ( has_post_thumbnail() ) { 
+							the_post_thumbnail('large'); 
+						}
+						else {
+							echo '<img src="' . get_bloginfo( 'stylesheet_directory' ) . '/assets/images/dsa_blog.png" />';
+						}
+				?>
+			</a>
+			
 		</div>
-		<header class="article-header large-8 medium-9 small-12 columns">
-			<h2 class="txt-bold"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-			<?php get_template_part( 'parts/content', 'byline' ); ?>
-		</header> <!-- end article header -->
+		<header class="archive-item-content large-9 medium-8 small-12 columns">
+			<h2 class="txt-bold archive-item-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+			<?php get_template_part( 'parts/content', 'byline' ); ?>	
+			<section class="archive-item-excerpt" itemprop="articleBody">
+				<?php the_excerpt(); ?>
+			</section> <!-- end archive-item-excerpt -->
+		</header> <!-- end archive-item-content -->
 	</div>
-					
-	<section class="entry-content" itemprop="articleBody">
-		
-		<?php the_content('<button class="button">' . __( 'Read more...', 'jointswp' ) . '</button>'); ?>
-	</section> <!-- end article section -->
-						
-	<footer class="article-footer">
-    	<p class="tags"><?php the_tags('<span class="tags-title">' . __('Tags:', 'jointstheme') . '</span> ', ', ', ''); ?></p>
-	</footer> <!-- end article footer -->	
-</article>
-<br> <!-- end article -->
+
+</article><!-- end article -->
