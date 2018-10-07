8c10e4f241ff56a2d7b45fca2390268196f806cf
David <dpeters911@hotmail.com>
2018-10-07 14:16:07 -0700

    Full Update 2018
    Bring in harmony with live site.

--- functions.php
+++ functions.php
@@ -35,65 +35,14 @@ add_action( 'save_post', 'myplugin_save_postdata' );
 /* Adds a box to the main column on the Post and Page edit screens */
 function myplugin_add_custom_box() {
   global $post;
-    if ( 'template-single-slide-one-column.php' == get_post_meta( $post->ID, '_wp_page_template', true ) ) {
-       		add_meta_box( 'wp_editor_test_1_box', 'Slide', 'wp_editor_meta_box_1' );
-  	}
-  	if ( 'template-single-slide-one-to-two-column.php' == get_post_meta( $post->ID, '_wp_page_template', true ) ) {
-       		add_meta_box( 'wp_editor_test_1_box', 'Slide', 'wp_editor_meta_box_1' );
-  			add_meta_box( 'wp_editor_test_3_box', 'DSA Left Column', 'wp_editor_meta_box_3' );
-  			add_meta_box( 'wp_editor_test_4_box', 'DSA Right Column', 'wp_editor_meta_box_4' );
-  	}
     if ( 'template-homepage-2017.php' == get_post_meta( $post->ID, '_wp_page_template', true ) ) {
           add_meta_box( 'wp_editor_test_8_box', 'DSA Feature Box', 'wp_editor_meta_box_8' );
           add_meta_box( 'wp_editor_test_4_box', 'Final Row', 'wp_editor_meta_box_4' );
           add_meta_box( 'wp_editor_test_11_box', 'DSA Alert Box', 'wp_editor_meta_box_11' );
     }
-  	if ( 'template-fullwidth-to-two-column.php' == get_post_meta( $post->ID, '_wp_page_template', true ) ) {
-  			add_meta_box( 'wp_editor_test_3_box', 'DSA Left Column', 'wp_editor_meta_box_3' );
-  			add_meta_box( 'wp_editor_test_4_box', 'DSA Right Column', 'wp_editor_meta_box_4' );
-  	}
-  	if ( 'template-two-column.php' == get_post_meta( $post->ID, '_wp_page_template', true ) ) {
-  			add_meta_box( 'wp_editor_test_4_box', 'DSA Right Column', 'wp_editor_meta_box_4' );
-  	}
-    if ( 'template-three-slides-to-two-columns.php' == get_post_meta( $post->ID, '_wp_page_template', true ) ) {
-        add_meta_box( 'wp_editor_test_3_box', 'DSA Left Column', 'wp_editor_meta_box_3' );
-        add_meta_box( 'wp_editor_test_4_box', 'DSA Right Column', 'wp_editor_meta_box_4' );
-        add_meta_box( 'wp_editor_test_5_box', 'Slide 1', 'wp_editor_meta_box_5' );
-        add_meta_box( 'wp_editor_test_6_box', 'Slide 2', 'wp_editor_meta_box_6' );
-        add_meta_box( 'wp_editor_test_7_box', 'Slide 3', 'wp_editor_meta_box_7' );
-    }
 }
 
 /* Prints the box content */
-function wp_editor_meta_box_1( $post ) {
-
-  // Use nonce for verification
-  wp_nonce_field( plugin_basename( __FILE__ ), 'myplugin_noncename' );
-
-  $field_value = get_post_meta( $post->ID, '_dsa_slide0', false );
-  wp_editor( $field_value[0], '_dsa_slide0' );
-
-}
-
-function wp_editor_meta_box_2( $post ) {
-
-  // Use nonce for verification
-  wp_nonce_field( plugin_basename( __FILE__ ), 'myplugin_noncename' );
-
-  $field_value = get_post_meta( $post->ID, '_dsa_slide_right', false );
-  wp_editor( $field_value[0], '_dsa_slide_right' );
-}
-
-function wp_editor_meta_box_3( $post ) {
-
-  // Use nonce for verification
-  wp_nonce_field( plugin_basename( __FILE__ ), 'myplugin_noncename' );
-
-  $field_value = get_post_meta( $post->ID, '_dsa_column_left', false );
-  wp_editor( $field_value[0], '_dsa_column_left' );
-}
-
-
 function wp_editor_meta_box_4( $post ) {
 
   // Use nonce for verification
@@ -103,33 +52,6 @@ function wp_editor_meta_box_4( $post ) {
   wp_editor( $field_value[0], '_dsa_column_right' );
 }
 
-function wp_editor_meta_box_5( $post ) {
-
-  // Use nonce for verification
-  wp_nonce_field( plugin_basename( __FILE__ ), 'myplugin_noncename' );
-
-  $field_value = get_post_meta( $post->ID, '_dsa_slide1', false );
-  wp_editor( $field_value[0], '_dsa_slide1' );
-}
-
-function wp_editor_meta_box_6( $post ) {
-
-  // Use nonce for verification
-  wp_nonce_field( plugin_basename( __FILE__ ), 'myplugin_noncename' );
-
-  $field_value = get_post_meta( $post->ID, '_dsa_slide2', false );
-  wp_editor( $field_value[0], '_dsa_slide2' );
-}
-
-function wp_editor_meta_box_7( $post ) {
-
-  // Use nonce for verification
-  wp_nonce_field( plugin_basename( __FILE__ ), 'myplugin_noncename' );
-
-  $field_value = get_post_meta( $post->ID, '_dsa_slide3', false );
-  wp_editor( $field_value[0], '_dsa_slide3' );
-}
-
 function wp_editor_meta_box_8( $post ) {
 
   // Use nonce for verification
@@ -137,24 +59,6 @@ function wp_editor_meta_box_8( $post ) {
 
   $field_value = get_post_meta( $post->ID, '_dsa_feature_box', false );
   wp_editor( $field_value[0], '_dsa_feature_box' );
-}
-
-function wp_editor_meta_box_9( $post ) {
-
-  // Use nonce for verification
-  wp_nonce_field( plugin_basename( __FILE__ ), 'myplugin_noncename' );
-
-  $field_value = get_post_meta( $post->ID, '_dsa_homepage_row_2', false );
-  wp_editor( $field_value[0], '_dsa_homepage_row_2' );
-}
-
-function wp_editor_meta_box_10( $post ) {
-
-  // Use nonce for verification
-  wp_nonce_field( plugin_basename( __FILE__ ), 'myplugin_noncename' );
-
-  $field_value = get_post_meta( $post->ID, '_dsa_homepage_row_3', false );
-  wp_editor( $field_value[0], '_dsa_homepage_row_3' );
 }
 
 function wp_editor_meta_box_11( $post ) {
@@ -193,37 +97,13 @@ function myplugin_save_postdata( $post_id ) {
   }
 
   // OK, we're authenticated: we need to find and save the data
-	if ( isset ( $_POST['_dsa_slide0'] ) ) {
-    update_post_meta( $post_id, '_dsa_slide0', $_POST['_dsa_slide0'] );
-  }
-
-  if ( isset ( $_POST['_dsa_column_left'] ) ) {
-    update_post_meta( $post_id, '_dsa_column_left', $_POST['_dsa_column_left'] );
-  }
 
   if ( isset ( $_POST['_dsa_column_right'] ) ) {
     update_post_meta( $post_id, '_dsa_column_right', $_POST['_dsa_column_right'] );
   }
 
-  if ( isset ( $_POST['_dsa_slide1'] ) ) {
-    update_post_meta( $post_id, '_dsa_slide1', $_POST['_dsa_slide1'] );
-  }
-
-  if ( isset ( $_POST['_dsa_slide2'] ) ) {
-    update_post_meta( $post_id, '_dsa_slide2', $_POST['_dsa_slide2'] );
-  }
-
-  if ( isset ( $_POST['_dsa_slide3'] ) ) {
-    update_post_meta( $post_id, '_dsa_slide3', $_POST['_dsa_slide3'] );
-  }
   if ( isset ( $_POST['_dsa_feature_box'] ) ) {
     update_post_meta( $post_id, '_dsa_feature_box', $_POST['_dsa_feature_box'] );
-  }
-   if ( isset ( $_POST['_dsa_homepage_row_2'] ) ) {
-    update_post_meta( $post_id, '_dsa_homepage_row_2', $_POST['_dsa_homepage_row_2'] );
-  }
-   if ( isset ( $_POST['_dsa_homepage_row_3'] ) ) {
-    update_post_meta( $post_id, '_dsa_homepage_row_3', $_POST['_dsa_homepage_row_3'] );
   }
    if ( isset ( $_POST['_dsa_alert_box'] ) ) {
     update_post_meta( $post_id, '_dsa_alert_box', $_POST['_dsa_alert_box'] );
--- template-homepage-2017.php
+++ template-homepage-2017.php
@@ -86,7 +86,7 @@ Template Name: Hompage 2017
 		          <div class="inner">
 		            <div class="pairing">
 		              <figure>
-		                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/ui-foregrounds/handshake.svg" alt="" />
+		                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/sdsa_logo_bg.svg" alt="" />
 		              </figure>
 		              <div class="form">
 		                <div class="fields">
