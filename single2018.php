bc546970e35f2b37d7fe745136a9902aa734de96
David <dpeters911@hotmail.com>
2018-10-07 14:37:32 -0700

    Begin 2018 Post Redesign
    Start of single.php redesign for better reading experience.

--- single2018.php
+++ single2018.php
@@ -1,30 +1,21 @@
 <?php
 /*
-This is the custom post type post template.
-If you edit the post type name, you've got
-to change the name of this template to
-reflect that name change.
-
-i.e. if your custom post type is called
-register_post_type( 'bookmarks',
-then your single template should be
-single-bookmarks.php
-
-*/
-?>
-
-<?php get_header(); ?>
+ * Template Name: Single Post (2018 Test)
+ * Template Post Type: post
+ */
+  
+ get_header(); ?>
 			
 <div id="content">
 
 	<div id="inner-content" class="row">
 
-		<main id="main" class="large-8 medium-8 columns first" role="main">
+		<main id="main" class="large-8 medium-8 columns" role="main">
 		
 		    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		
 		    	<?php get_template_part( 'parts/loop', 'single' ); ?>
-		    					
+		    	
 		    <?php endwhile; else : ?>
 		
 		   		<?php get_template_part( 'parts/content', 'missing' ); ?>
