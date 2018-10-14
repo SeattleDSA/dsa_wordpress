0af1b88ce5891953de371f350b00eec558606d14
David <dpeters911@hotmail.com>
2018-10-13 17:49:45 -0700

    Adjustments to Single.php
    Offsets sidebar to appear off sreen on desktop and tablet.

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,4 @@
-<div id="sidebar1" class="sidebar columns large-4" role="complementary">
+<div id="sidebar1" class="sidebar columns large-4 medium-4" role="complementary">
 
 	<?php if ( is_active_sidebar( 'sidebar1' ) ) : ?>
 
