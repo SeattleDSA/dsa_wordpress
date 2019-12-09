251272f23a9129ab7db1f8385a32d0e4a6b138d0
David <dpeters911@hotmail.com>
2019-12-08 16:13:10 -0800

    Documentation Page Template
    Adds tocbot support to documentation page template, which automatically adds a table of contents based on Headings in a page's content.
    
    See more info and documentation at:
    https://github.com/tscanlin/tocbot

---
+++ parts/loop-documentation.php
@@ -0,0 +1,11 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> role="article" itemscope itemtype="http://schema.org/WebPage">
+						
+	<header class="article-header">
+		<h1 class="page-title"><?php the_title(); ?></h1>
+	</header> <!-- end article header -->
+					
+    <section class="entry-content" itemprop="articleBody">
+	    <?php the_content(); ?>
+	</section> <!-- end article section -->
+					
+</article> <!-- end article -->
