3e9f6cd75ca0f6a7dfc2d4e5542cc9f005ea4c09
David <dpeters911@hotmail.com>
2019-12-08 19:24:05 -0800

    Blog post changes
    Whitespace no longer dependent on viewpoint height and uses rem height. Certain functions break using viewport height.

--- single.php
+++ single.php
@@ -4,7 +4,7 @@
 
 	<div id="inner-content" class="grid-x grid-margin-x">
 
-		<main id="main" class="cell large-8 medium-8" role="main">
+		<main id="main" class="cell large-8 medium-8 blog-post" role="main">
 		
 		    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		
