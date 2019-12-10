60f58e7cde1470c5b2f2bd3ba1cd087e4b2e5273
David <dpeters911@hotmail.com>
2019-12-09 21:08:03 -0800

    Gradient Animations on Buttons
    Higher contrast, more noticeable changes on hover state. Also reduced code somehow.

--- parts/content-events.php
+++ parts/content-events.php
@@ -8,8 +8,8 @@ Use:	Shares platform of beliefs, similar to Black Panther Program, as well as po
 <div class="grid-container homepage-events">
 	<div class="grid-x grid-margin-x grid-margin-y txt-dsa-white">
 		<div class="cell large-2 large-offset-5 medium-4 medium-offset-4 small-6 small-offset-3">
-			<a href="<?php echo home_url(); ?>/events/">
-				<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/white/calendar.svg" class="homepage-icon" />
+			<a href="<?php echo home_url(); ?>/events/" class="homepage-icon">
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/white/calendar.svg" />
 			</a>
 		</div>
 		<br />
@@ -46,7 +46,7 @@ Use:	Shares platform of beliefs, similar to Black Panther Program, as well as po
 					    		<div class="cell large-12 medium-6 small-12">
 						    		<div class="grid-x grid-margin-x">
 							    		<div class="cell large-2 medium-4 small-3">
-					    					<a href="<?php echo tribe_get_event_link ( $event->ID  ); ?>">
+					    					<a href="<?php echo tribe_get_event_link ( $event->ID  ); ?>" class="homepage-icon">
 					    						<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/white/calendar.svg" class="dsa-calendar-icon" />
 					    					</a>
 					    			 	</div>
@@ -54,7 +54,7 @@ Use:	Shares platform of beliefs, similar to Black Panther Program, as well as po
 					    					<p><?php echo tribe_events_event_schedule_details( $event->ID ); ?></p>
 					    				</div>
 					    				<div class="cell large-2 medium-4 small-3">
-					    					<a href="<?php echo tribe_get_event_link ( $event->ID  ); ?>">
+					    					<a href="<?php echo tribe_get_event_link ( $event->ID  ); ?>" class="homepage-icon">
 					    						<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/white/location.svg" class="dsa-calendar-icon" />
 					    					</a>
 					    				</div>
