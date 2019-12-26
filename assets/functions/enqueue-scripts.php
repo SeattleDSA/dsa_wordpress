d0a1c1d0f60f2d57f2d0ec48304b5db4d6ad8657
David <dpeters911@hotmail.com>
2019-12-25 23:26:17 -0800

    Print Stylesheet Fixes
    Forces blocks and reduces print sizes

--- assets/functions/enqueue-scripts.php
+++ assets/functions/enqueue-scripts.php
@@ -16,7 +16,7 @@ function site_scripts() {
     wp_enqueue_style( 'inter-typeface', get_template_directory_uri() . '/vendor/inter/inter.css', array(), '3.11', 'all' );
     wp_enqueue_style( 'share-icons', get_template_directory_uri() . '/vendor/iconmoon/share.css', array(), '', 'all' );
     // Register main stylesheet
-    wp_enqueue_style( 'site-css', get_template_directory_uri() . '/assets/css/style.min.css', array(), '1.05', 'all' );
+    wp_enqueue_style( 'site-css', get_template_directory_uri() . '/assets/css/style.min.css', array(), '1.07', 'all' );
 
     // Comment reply script for threaded comments
     if ( is_singular() AND comments_open() AND (get_option('thread_comments') == 1)) {
