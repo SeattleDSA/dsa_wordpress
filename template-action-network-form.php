8f006ba209bcad57847f6feb30620af656978318
Dave Benvenuti <davebenvenuti@gmail.com>
2020-06-11 23:26:03 -0700

    Added template for embedding Action Network forms

---
+++ template-action-network-form.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Template Name: Action Network Form
+*/
+?>
+
+<?php get_header(); ?>
+  <style>
+    .wp-block-embed__wrapper { display: none; }
+    .article-header { display: none; }
+    #inner-content { text-align: center; }
+    #main { margin: auto; }
+  </style>
+
+  <div id="content" class="grid-container fluid">
+
+    <div id="inner-content" class="grid-x grid-margin-x">
+
+        <main id="main" class="cell medium-8 large-9 dsa-readable" role="main">
+
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+          <?php get_template_part( 'parts/loop', 'documentation' ); ?>
+
+        <?php endwhile; endif; ?>
+
+      </main> <!-- end #main -->
+
+    </div> <!-- end #inner-content -->
+
+  </div> <!-- end #content -->
+
+<?php get_footer(); ?>
