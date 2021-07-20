4cdf9a9f74be3639a75d064885eb4db4abcad3b5
David <dpeters0911@gmail.com>
2021-07-19 20:15:23 -0700

    Update cleanup.php
    Fix the read more link

--- assets/functions/cleanup.php
+++ assets/functions/cleanup.php
@@ -71,7 +71,7 @@ function joints_gallery_style($css) {
 function joints_excerpt_more($more) {
 	global $post;
 	// edit here if you like
-return '<a class="excerpt-read-more" href="'. get_permalink($post->ID) . '" title="'. __('Read', 'jointswp') . get_the_title($post->ID).'">'. __('... Read more &raquo;', 'jointswp') .'</a>';
+return '<a class="excerpt-read-more-link" href="'. get_permalink($post->ID) . '" title="'. __('Read', 'jointswp') . get_the_title($post->ID).'"> '. __('... Read more', 'jointswp') .'</a>';
 }
 
 //  Stop WordPress from using the sticky class (which conflicts with Foundation), and style WordPress sticky posts using the .wp-sticky class instead
