7a26018224f536eae8887f6cdaa68ac4ca93faa1
David <dpeters0911@gmail.com>
2021-08-06 15:34:58 -0700

    Prep for 2021 Homepage
    Block editor support eventually

---
+++ template-homepage-2021.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Template Name: Hompage 2021
+*/
+?>
+
+<?php get_header(); ?>
+
+	<div id="content">
+		<?php get_template_part( 'parts/content', 'alert' ); ?> <!-- see "DSA Alert Box" metabox in the Page Editor or customize in /parts/content-dsaAlertBox.php -->
+		<div class="grid-container grid-x featuredDioramaContainer">
+			<?php if( !empty(get_the_post_thumbnail()) ) { ?>
+			    <?php the_post_thumbnail('large');?>
+			<?php } else { ?>
+			    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/ui-foregrounds/mobile-mural.png" class="featuredDiorama" />
+			<?php } ?>
+		</div>
+		<div class="grid-container grid-x grid-margin-x">
+			<div class="cell large-12 medium-12 small-12"><!-- Begin Main Content-->
+		  	  	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+					<?php get_template_part( 'parts/loop', 'pagealt' ); ?> 
+				<?php endwhile; endif; ?>	
+			</div>
+		</div>	
+	</div> <!-- end #content -->
+
+<?php get_footer(); ?>
