d6a0c2f4c5e278156134212ae3e51f464aeaccac
David <dpeters0911@gmail.com>
2023-01-07 08:00:56 -0800

    Update enqueue-scripts.php
    updates to functions

--- assets/functions/enqueue-scripts.php
+++ assets/functions/enqueue-scripts.php
@@ -13,7 +13,7 @@ function site_scripts() {
     wp_enqueue_script( 'site-js', get_template_directory_uri() . '/assets/js/scripts.js', array( 'jquery' ), '', true );
 
     // Stylesheets
-    wp_enqueue_style( 'foundation-css', get_template_directory_uri() . '/vendor/foundation-sites/dist/css/foundation.min.css', array(), '6.6.3', 'all' ); 
+    wp_enqueue_style( 'foundation-css', get_template_directory_uri() . '/vendor/foundation-sites/dist/css/foundation.min.css', array(), '6.7.5', 'all' ); 
 
     wp_enqueue_style( 'justice-icons', get_template_directory_uri() . '/vendor/justice-icons/justice-icons.css', array(), '', 'all' );
     // Register main stylesheet
